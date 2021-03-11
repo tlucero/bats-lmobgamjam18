f7c91c6a8072c085b002e6c591d58e77df3f8f3c
Siddhant Rathor <cdsid10@gmail.com>
2021-03-12 01:36:37 +0530

    Level 2 second pass

---
+++ Assets/WinTele.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTele : MonoBehaviour
+{
+    [SerializeField] private int scenes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(scenes);
+        }
+    }
+}
